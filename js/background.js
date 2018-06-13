16b146b66043ae35f85c3690cedf9d971bd53233
asins <wkli@alibaba-inc.com>
2018-06-13 22:17:21 +0800

    解决多窗口时获取当前窗口URL不正确问题

--- js/background.js
+++ js/background.js
@@ -2,3 +2,26 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	// 显示小图标
 	chrome.pageAction.show(tabId);
 });
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+	if(request.type == 'img'){ // 获取图片二进制内容
+		getImgForXhr(request.url, function(res){
+			sendResponse(res);
+		});
+	}
+});
+
+
+function getImgForXhr(url, callback){
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.responseType = "blob";
+	xhr.onreadystatechange = function () {
+		if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+			callback(URL.createObjectURL(xhr.response));
+			// callback(xhr.responseText);
+		}
+		xhr.onreadystatechange = null
+	}
+	xhr.send();
+}
